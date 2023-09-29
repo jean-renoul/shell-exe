fd76a019a384c0e5ce17fb55f3d1b27ba04a6db5
jean-renoul <jean.renoul@laplateforme.io>
2023-09-29 03:11:37 +0200

    Job09

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -1,9 +1,29 @@
-filename="Shell_Userlist.csv"
+#!/bin/bash
+set -x
 
-while read data
+filename='Shell_Userlist.csv'
 
-do
 
-echo $data
+declare -a id
+declare -a prenom
+declare -a nom
+declare -a mdp
+declare -a role
 
-done < $filename
+sed -i 's/ *, */,/g' "Shell_Userlist.csv"
+
+while IFS=, read -r COL1 COL2 COL3 COL4 COL5 THRASH; do
+  id+=("$COL1")
+  prenom+=("$COL2")
+  nom+=("$COL3")
+  mdp+=("$COL4")
+  role+=("$(echo "$COL5" | tr -d '\r')")
+done < "$filename"
+
+for index in "${!role[@]}"; do
+  useradd -u "${id[$index]}" -d "/home/${prenom[$index]}" -m -s /bin/bash -p "$(echo "${mdp[$index]}" | openssl passwd -1 -stdin)" "${prenom[$index]}"
+if [[ "${role[$index]}" == "Admin" ]]; then usermod -aG root "${prenom[$index]}"
+fi
+done
+
+echo "Roles: ${role[@]}"
